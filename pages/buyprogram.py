09546d2d74a2e8b7cfad08495bcc7a540b66417a
kyusung-blip <kyusung@seobukinter.com>
2026-02-26 11:24:17 +0900

    Add import for ecountenter in buyprogram.py

--- pages/buyprogram.py
+++ pages/buyprogram.py
@@ -14,6 +14,7 @@ import inventoryenter
 import Inspectioncheck
 import socket
 import ecount
+import ecountenter
 import google_sheet_manager as gsm
 from st_copy_to_clipboard import st_copy_to_clipboard
 import cyberts_crawler
